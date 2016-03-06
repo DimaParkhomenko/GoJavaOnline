58edaea8422dea77abc164f540059bd5b5337e7d
Dima Parkhomenko <dimin13111986@gmail.com>
2016-03-06 22:36:56 +0200

    Convert temp. and calculate dist.

--- src/module4/ex2_temperature/ConvertsTemperature.java
+++ src/module4/ex2_temperature/ConvertsTemperature.java
@@ -5,4 +5,22 @@ package module4.ex2_temperature;
  * в значение по шкале Фаренгейта и в обратном направлении.
  */
 public class ConvertsTemperature {
+    public static void main(String[] args) {
+        int celsiusDegree = 21;
+        System.out.println(celsiusDegree + " градусов Цельсия по шкале Фаренгейта равняется " + celsiusToFahrenheit(celsiusDegree) + ";");
+
+        int fahrenheitDegree = 263;
+        System.out.println(fahrenheitDegree + " градусов Фаренгейтая по шкале Цельсия равняется " + Math.round(fahrenheitToCelsius(fahrenheitDegree)) + ";");
+    }
+    public static double celsiusToFahrenheit(int celsiusDegree) {
+        double convertCelsiusToFahrenheit = celsiusDegree * 33.8;
+        return convertCelsiusToFahrenheit;
+    }
+
+    public static double fahrenheitToCelsius(int celsiusDegree) {
+        double convertFahrenheitToCelsius = celsiusDegree / 33.8;
+        return convertFahrenheitToCelsius;
+    }
 }
+
+
--- src/module4/ex3_distance/CalculateDistance.java
+++ src/module4/ex3_distance/CalculateDistance.java
@@ -5,4 +5,16 @@ package module4.ex3_distance;
  * координатами в двумерном пространстве (x1, y1), (x2, y2).
  */
 public class CalculateDistance {
+    public static void main(String[] args) {
+        int x1 = 2;
+        int x2 = 9;
+        int y1 = 5;
+        int y2 = 8;
+        System.out.println("Pасстояние между двумя точками " + "A(" + x1 + ";" + y1 + ") и " + "B(" + x2 + ";" + y2 + ") "+ "равняется " + distanceBetweenTwoPoints(x1, y1, x2, y2));
+    }
+
+    public static int distanceBetweenTwoPoints(int x1, int y1, int x2, int y2) {
+        int distance = (int) Math.sqrt(Math.pow(2, (x2 - x1)) + Math.pow(2, (y2 - y1)));
+        return distance;
+    }
 }
