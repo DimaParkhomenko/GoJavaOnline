fc471eac88dfb3ea6313429ec3e51a16daa21ac2
martazobro <marta.zobro@perfectial.com>
2016-03-14 23:09:57 +0200

    please see comments in code describing changes

--- src/module6/FileTestDrive.java
+++ src/module6/FileTestDrive.java
@@ -3,54 +3,53 @@ package module6;
 import java.util.Scanner;
 
 public class FileTestDrive {
-    public static void main(String[] args) {
-        Directory directory = new Directory();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
-        try {
-            System.out.println("Gues what type of file is right:");
-            System.out.println("These are our variants: ");
-            System.out.println("AudioFile");
-            System.out.println("ImgFile");
-            System.out.println("TextFile");
-            System.out.println("Media");
-            System.out.println("Your answer");
-            System.out.println();
-            System.out.println("Write your answer:");
-            String answer = scanner.nextLine();
-            if (answer.contains("AudioFile") || answer.contains("ImgFile") || answer.contains("TextFile")) {
-                System.out.println("Wright answer!!!");
-            } else {
-                throw new WrongCharException(answer);
-            }
-        } catch (WrongCharException e) {
-            System.out.println("Fatal Error: you'v wrote " + "\"" + e.getType() + "\"" + " and it is wrong answer so we will die!!!  :)))");
-        }
-        System.out.println();
 
-        File[] files = new File[3];
-        files[0] = new AudioFile();
-        files[1] = new TextFile();
-        files[2] = new ImgFile();
-        String[] arrCheckSizeOfKeeping = new String[3];
-        arrCheckSizeOfKeeping[0] = directory.getAudioFile();
-        arrCheckSizeOfKeeping[1] = directory.getImgFile();
-        arrCheckSizeOfKeeping[2] = directory.getTextFile();
+	public static void main(String[] args) {
+		// extracted logic into different methods to make it easier to read.
+		testConsoleReadNoExceptions();
+		testDirectory();
 
-        System.out.println("There such types of files in Directory as:");
-        try {
-            for (int i = 0; i < files.length; i++) {
-                files[i].printFileDetails();
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+	}
 
+	private static void testConsoleReadNoExceptions() {
+		try {
+			readUsersAnswer();
+		} catch (WrongCharException e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+	}
 
-        System.out.println();
-        System.out.println("Some others information about files:");
-        System.out.println("AudioFile - max size of keeping is " + directory.getAudioFile() + ";");
-        System.out.println("TextFile - max size of keeping is " + directory.getImgFile() + ";");
-        System.out.println("ImgFile - max size of keeping is " + directory.getTextFile() + ".");
-    }
+	private static void readUsersAnswer() throws WrongCharException {
+		// this is auto-closable try-catch, it will close resources opened in
+		// try()
+		try (Scanner scanner = new Scanner(System.in);) {
+			System.out.println("Gues what type of file is right:");
+			System.out.println("These are our variants: ");
+			System.out.println("AudioFile");
+			System.out.println("ImgFile");
+			System.out.println("TextFile");
+			System.out.println("Media");
+			System.out.println("Your answer");
+			System.out.println();
+			System.out.println("Write your answer:");
+			String answer = scanner.nextLine();
+			if (answer.contains("AudioFile") || answer.contains("ImgFile")
+					|| answer.contains("TextFile")) {
+				System.out.println("Wright answer!!!");
+			} else {
+				throw new WrongCharException("Fatal Error: you'v wrote " + "\""
+						+ answer + "\""
+						+ " and it is wrong answer so we will die!!!  :)))");
+			}
+		}
+	}
+
+	private static void testDirectory() {
+		Directory directory = new Directory();
+		directory.putFile(new AudioFile());
+		directory.putFile(new TextFile());
+		directory.putFile(new ImgFile());
+		directory.printFilesDetails();
+	}
 }
--- src/module6/WrongCharException.java
+++ src/module6/WrongCharException.java
@@ -1,17 +1,24 @@
 package module6;
 
 public class WrongCharException extends Exception {
-    private String type;
 
-    public WrongCharException(String type) {
-        this.type = type;
-    }
+	// Exception is designed to keep message and other exceptions details
+	// When we create new Exception's class, usually we initialize all types of
+	// constructor as Exception class has. 
+	public WrongCharException(String message) {
+		super(message);
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public WrongCharException() {
+		super();
+	}
 
-    public String getType() {
-        return type;
-    }
+	public WrongCharException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public WrongCharException(Throwable cause) {
+		super(cause);
+	}
+
 }
