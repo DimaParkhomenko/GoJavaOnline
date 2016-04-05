4bb083daedc252c73662f4e0bfe1589afc00a4b3
Dima Parkhomenko <dimin13111986@gmail.com>
2016-04-05 13:27:26 +0300

    Ref. Module 10.

--- src/module10/CaesarCipherTestDrive.java
+++ src/module10/CaesarCipherTestDrive.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class CaesarCipherTestDrive {
     public static void main(String[] args) throws IOException {
+
+        CeasarCipher ceasarCipher = new CeasarCipher();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Print text:");
         String text = scanner.nextLine();
@@ -13,78 +15,11 @@ public class CaesarCipherTestDrive {
         System.out.println();
         System.out.println("Caesar Cipher in work:");
 
-        try (FileWriter writer = new FileWriter("Text.txt")) {
-            writer.write(encrypt(text, key));
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println("Encrypt: " + ex.getMessage());
-        }
-
-        try (FileReader reader = new FileReader("Text.txt")) {
-            int c;
-            while ((c = reader.read()) != -1) {
-                System.out.print((char) c);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
+        FileUtil fileUtil = new FileUtil();
+        fileUtil.write("Text.txt", ceasarCipher.encrypt(text, key));
+        fileUtil.read("Text.txt");
         System.out.println();
-
-        try (FileWriter writer = new FileWriter("Text.txt")) {
-            writer.write(decrypt(encrypt(text, key), key));
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println("Decrypt: " + ex.getMessage());
-        }
-
-        try (FileReader reader = new FileReader("Text.txt")) {
-            int c;
-            while ((c = reader.read()) != -1) {
-                System.out.print((char) c);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public static String encrypt(String str, int keyLength) {
-        String encrypted = "";
-        for (int i = 0; i < str.length(); i++) {
-            int c = str.charAt(i);
-            if (Character.isUpperCase(c)) {
-                c = c + (keyLength % 26);
-                if (c > 'Z')
-                    c = c - 26;
-            } else if (Character.isLowerCase(c)) {
-                c = c + (keyLength % 26);
-
-                if (c > 'z')
-                    c = c - 26;
-            }
-            encrypted = encrypted + (char) c;
-        }
-        return encrypted;
-    }
-
-    public static String decrypt(String str, int keyLength) {
-        String decrypted = "";
-        for (int i = 0; i < str.length(); i++) {
-
-            int c = str.charAt(i);
-            if (Character.isUpperCase(c)) {
-                c = c - (keyLength % 26);
-
-                if (c < 'A')
-                    c = c + 26;
-            } else if (Character.isLowerCase(c)) {
-                c = c - (keyLength % 26);
-
-                if (c < 'a')
-                    c = c + 26;
-            }
-            decrypted = decrypted + (char) c;
-        }
-        return decrypted;
+        fileUtil.write("Text.txt", ceasarCipher.decrypt(ceasarCipher.encrypt(text, key), key));
+        fileUtil.read("Text.txt");
     }
 }
